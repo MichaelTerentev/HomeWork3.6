046c2b548a86c3f428ee944ee129599fb41f3032
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-12 19:06:50 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+		MyQueue<String> queue = new MyQueue<>(Comparator.comparing(String::length));
+		queue.enqueue("long");
+		queue.enqueue("the longest");
+		queue.enqueue("longer");
+		queue.dequeue();
+		
+		for(int i = 0; i < queue.size(); i++) {
+			System.out.println(queue.get(i));
+        }
     }
 }
